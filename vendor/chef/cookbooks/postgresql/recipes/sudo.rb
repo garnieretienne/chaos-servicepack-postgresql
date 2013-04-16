5dacf43dce414b097e04c8e09adbec3bb8f346c7
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-16 19:09:25 +0200

    add sudo rule

---
+++ vendor/chef/cookbooks/postgresql/recipes/sudo.rb
@@ -0,0 +1,9 @@
+# Sudo rule to allow addons user to execute psql commands
+template "postgresql sudo" do
+  path "/etc/sudoers.d/addons"
+  source "sudo.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  action :create
+end
